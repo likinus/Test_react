23b339f6aa74e887ee41f6614c3debc1ee264876
Likinus <likinus2@gmail.com>
2020-07-11 13:00:09 +0300

    new react app

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import TodoList from './Todo/TodoList';
+import Context from './context'
+
+function App() {
+  const [todos, setTodos] = React.useState([
+  {id: 1, completed: false, title: 'Buy bread'},
+  {id: 2, completed: false, title: 'Buy Butter'},
+  {id: 3, completed: false, title: 'Buy milk'}
+  ])
+  
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+      })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <div className='wrapper'>
+        <h1>React Tutorial</h1>
+
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}></TodoList> : <p>No todos</p>}
+        
+      </div>
+    </Context.Provider>
+  );
+}
+
+export default App;
---
+++ src/Todo/TodoItem.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react'
+import Proptypes from 'prop-types'
+import Context from '../context'
+
+const styles = {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        aligntItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem'
+    },
+    input: {
+        marginRight: '1rem'
+    }
+}
+
+function TodoItem({ todo, index, onChange }) {
+    const {removeTodo} = useContext(Context)
+    const classes = []
+
+    if (todo.completed) {
+        classes.push('done')
+    }
+    return (
+        <li style={styles.li}>
+            <span className={classes.join(' ')}>
+                <input 
+                checked={todo.completed}
+                type="checkbox" 
+                style={styles.input} 
+                onChange={() => onChange(todo.id)}></input>
+                <strong>{index + 1}</strong>
+                &nbsp;
+                {todo.title}
+            </span>
+
+            <button className='rm' onClick={() => removeTodo(todo.id)}>&times;</button>
+        </li>
+    )
+}
+
+TodoItem.propTypes = {
+    todo: Proptypes.object.isRequired,
+    index: Proptypes.number,
+    onChange: Proptypes.func.isRequired
+}
+
+export default TodoItem
